289c134d5fc69e92d65bfb90e49312a4711b36ee
NifatHossain <nifathossain1480@gmail.com>
2024-09-22 09:08:33 +0600

    Added Navbar

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+
+const HomePage = () => {
+    return (
+        <div>
+            <NavigationBar></NavigationBar>
+        </div>
+    );
+};
+
+export default HomePage;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,11 @@
 import {
     createBrowserRouter,
 } from "react-router-dom";
+import HomePage from "../pages/HomePage";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <HomePage></HomePage>
     },
 ]);
